5013fbc6b52117b3ff50fd68d7e899fb16cc3470
HamedPour <hamed_k.pour@hotmail.com>
2020-12-25 09:09:29 +0000

    Updates

---
+++ precache-manifest.2c8167652009e0710b65be41b87b4cc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3671b68e4cf69ac0308a4e0c8fb06594",
+    "url": "/react-piano/index.html"
+  },
+  {
+    "revision": "8860a72652133da691de",
+    "url": "/react-piano/static/js/2.453a0c13.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-piano/static/js/2.453a0c13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3cd60cac6a44b98fddb",
+    "url": "/react-piano/static/js/main.bfb7c9d9.chunk.js"
+  },
+  {
+    "revision": "71083feebefa402a2d69",
+    "url": "/react-piano/static/js/runtime-main.640cde7b.js"
+  }
+]);
